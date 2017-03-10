256e5338451bb71b59226362fc8c7fcf2ce1642e
VVoev <vladovoev@yahoo.co.uk>
2017-03-10 22:04:47 +0200

    Js initial commit

---
+++ Hangman/JavaScript/script.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    var categories;         // Array of topics
+    var chosenCategory;     // Selected catagory
+    var getHint;          // Word getHint
+    var word;              // Selected word
+    var guess;             // Geuss
+    var geusses = [];      // Stored geusses
+    var lives;             // Lives
+    var counter;           // Count correct geusses
+    var space;              // Number of spaces in word '-'
+
+    // Get elements
+    var showLives = document.getElementById("mylives");
+    var showCatagory = document.getElementById("scatagory");
+         getHint = document.getElementById("hint");
+    var showClue = document.getElementById("clue");
+
+
+}
+
+
+
